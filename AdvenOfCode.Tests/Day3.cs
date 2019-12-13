15da4b67f394dfb3ce9a69864322d7c008dad6f0
Jupotter <jupotter@jupotter.eu>
2019-12-13 17:03:32 +0100

    Start of day3

---
+++ AdvenOfCode.Tests/Day3.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day3
+    {
+        [Fact]
+        public void TestNoCrossing()
+        {
+            var instructions = new[] {new[] {"U3"}, new[] {"L3"}};
+            var results      = AdventOfCode.Day3.FindIntersections(instructions);
+            results.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void TestOneCrossing()
+        {
+            var instructions = new[] {new[] {"U2", "L3"}, new[] {"L2", "U3"}};
+            var results      = AdventOfCode.Day3.FindIntersections(instructions);
+            results.Should().HaveCount(1).And.Contain(new Point(-2, 2));
+        }
+
+        [Fact]
+        public void TestTwoCrossings()
+        {
+            var instructions = new[] {new[] {"R8", "U5", "L5", "D3"}, new[] {"U7", "R6", "D4", "L4"}};
+            var results      = AdventOfCode.Day3.FindIntersections(instructions);
+            results.Should().HaveCount(2).And.Contain(new[] {new Point(3, 3), new Point(6, 5)});
+        }
+
+        [Fact]
+        public void TestNoOwnCrossings()
+        {
+            var instructions = new[] { new[] { "R8", "U5", "L5", "D10" } };
+            var results      = AdventOfCode.Day3.FindIntersections(instructions);
+            results.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void TestClosestInOneCrossing()
+        {
+            var instructions = new[] {new[] {"U2", "L3"}, new[] {"L2", "U3"}};
+            var results      = AdventOfCode.Day3.FindClosestCrossing(instructions);
+            results.Should().Be(new Point(-2, 2));
+        }
+
+        [Fact]
+        public void TestClosestInTwoCrossing()
+        {
+            var instructions = new[] {new[] {"R8", "U5", "L5", "D3"}, new[] {"U7", "R6", "D4", "L4"}};
+            var results      = AdventOfCode.Day3.FindClosestCrossing(instructions);
+            results.Should().Be(new Point(3, 3));
+        }
+
+        [Theory]
+        [InlineData("R8,U5,L5,D3\nU7,R6,D4,L4", 6)]
+        [InlineData("R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83", 159)]
+        [InlineData("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 135)]
+        public void FindClosestCrossing(string instruction, int distance)
+        {
+            AdventOfCode.Day3.FindClosestCrossingDistance(instruction).Should().Be(distance);
+        }
+    }
+}
---
+++ AdventOfCode/Day3.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace AdventOfCode
+{
+    public struct Point : IEquatable<Point>
+    {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; }
+        public int Y { get; }
+
+        public bool Equals(Point other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Point other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
+
+        public static Point operator +(Point first, Point second)
+        {
+            return new Point(first.X + second.X, first.Y+second.Y);
+        }
+    }
+
+    public class Day3
+    {
+        public static IEnumerable<Point> FindIntersections(IEnumerable<IEnumerable<string>> instructions)
+        {
+            var intersections = new List<Point>();
+            var lines = new HashSet<Point>();
+            bool first = true;
+            foreach (var line in instructions)
+            {
+                Point current = new Point();
+                foreach (string instruction in line)
+                {
+                    var direction = instruction[0];
+                    var size = int.Parse(instruction.Substring(1));
+                    var iteration = direction switch
+                    {
+                        'U' => new Point(0, 1),
+                        'D' => new Point(0, -1),
+                        'L' => new Point(-1, 0),
+                        'R' => new Point(1, 0),
+                        _   => throw new InvalidOperationException()
+                    };
+
+                    for (int i = 0; i < size; i++)
+                    {
+                        current += iteration;
+                        if (lines.Contains(current) && !first)
+                        {
+                            intersections.Add(current);
+                        }
+                        else
+                        {
+                            lines.Add(current);
+                        }
+                    }
+                }
+
+                first = false;
+            }
+            return intersections;
+        }
+
+        public static Point FindClosestCrossing(IEnumerable<IEnumerable<string>> instructions)
+        {
+            var intersections = FindIntersections(instructions);
+            return intersections.OrderBy(p => p.X + p.Y).First();
+        }
+
+        public static Point FindClosestCrossing(string instructions)
+        {
+            var instructionList = instructions.Split('\n').Select(s => s.Split(','));
+            return FindClosestCrossing(instructionList);
+        }
+
+        public static int FindClosestCrossingDistance(string instructions)
+        {
+            var crossing = FindClosestCrossing(instructions);
+            return crossing.X + crossing.Y;
+        }
+    }
+}
