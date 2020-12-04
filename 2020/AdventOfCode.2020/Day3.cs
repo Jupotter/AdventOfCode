7a7e75f94bef28229033ae24e6ab86d0f3246895
Jupotter <jupotter@jupotter.eu>
2020-12-04 11:55:11 +0100

    Day3

---
+++ 2020/AdventOfCode.2020/Day3.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using AoCHelper;
+
+namespace AdventOfCode2020
+{
+    public sealed class Day3 : BaseDay
+    {
+        private readonly bool[,] data;
+
+        public Day3()
+        {
+            using var sr   = new StreamReader(InputFilePath);
+            data = LoadTrees(sr.ReadToEnd());
+        }
+
+        public override string Solve_1() => FindCollisions((3, 1), data).ToString();
+
+        public override string Solve_2()
+        {
+            var slopes = new[]
+                         {
+                             (x: 1, y: 1),
+                             (x: 3, y: 1),
+                             (x: 5, y: 1),
+                             (x: 7, y: 1),
+                             (x: 1, y: 2),
+                         };
+
+            return slopes.Select(s => FindCollisions(s, data)).Aggregate((x, y) => x * y).ToString();
+        }
+
+        public static bool[,] LoadTrees(string source)
+        {
+            source = source.Trim();
+            var lines  = source.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            var length = lines[0].Length;
+            var result = new bool[lines.Length, length];
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                for (int j = 0; j < result.GetLength(1); j++)
+                {
+                    result[i, j] = (lines[i][j] == '#');
+                }
+            }
+
+            return result;
+        }
+
+        public static int FindCollisions((int x, int y) slope, bool[,] trees)
+        {
+            var width  = trees.GetLength(1);
+            var height = trees.GetLength(0);
+            var count  = 0;
+            var pos    = (x: 0, y: 0);
+            while (pos.y < height)
+            {
+                if (trees[pos.y, pos.x])
+                {
+                    count++;
+                }
+
+                pos.x += slope.x;
+                pos.y += slope.y;
+                if (pos.x >= width)
+                {
+                    pos.x -= width;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ AdventOfCode.2020.Tests/Day3Test.cs
@@ -0,0 +1,53 @@
+﻿using Xunit;
+
+namespace AdventOfCode2020.Tests
+{
+    public class Day3Test
+    {
+        private const string SampleData = @"
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#
+";
+
+        [Fact]
+        public void Solve1TestFromWeb()
+        {
+            var data   = Day3.LoadTrees(SampleData);
+            var result = Day3.FindCollisions((3, 1), data);
+
+            Assert.Equal(7, result);
+        }
+
+        [Fact]
+        public void LoadTrees()
+        {
+            var expected = new[,]
+                           {
+                               {false, false, true, true, false, false, false, false, false, false, false},
+                               {true, false, false, false, true, false, false, false, true, false, false},
+                               {false, true, false, false, false, false, true, false, false, true, false},
+                           };
+            var result = Day3.LoadTrees(SampleData);
+
+            Assert.Equal(11, result.GetLength(0));
+            Assert.Equal(11, result.GetLength(1));
+
+            for (int i = 0; i < expected.GetLength(0); i++)
+            {
+                for (int j = 0; j < expected.GetLength(1); j++)
+                {
+                    Assert.Equal(expected[i, j], result[i, j]);
+                }
+            }
+        }
+    }
+}
