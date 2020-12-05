65e6f6f4684d1d964302c35bc56cf3562c468391
Jupotter <jupotter@jupotter.eu>
2020-12-05 09:37:40 +0100

    Move unit test project to 2020 folder

---
+++ 2020/AdventOfCode.2020.Tests/Day1Test.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace AdventOfCode2020.Tests
+{
+    public class Day1Test
+    {
+        [Fact]
+        public void SampleTest()
+        {
+            var tested = new Day1();
+            var data = new[]
+                       {
+                           1721,
+                           979,
+                           366,
+                           299,
+                           675,
+                           1456
+                       };
+
+            var result = tested.GetProductOf2020(data);
+            var result2 = tested.GetProductOf2020_2(data);
+
+            Assert.Equal(514579,    result);
+            Assert.Equal(241861950, result2);
+        }
+    }
+}
---
+++ 2020/AdventOfCode.2020.Tests/Day2Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AdventOfCode2020.Tests
+{
+    public class Day2Test
+    {
+        [Fact]
+        public void Solve1TestFromWeb()
+        {
+            var tested = new Day2();
+            var data = new[]
+                       {
+                           "1-3 a: abcde",
+                           "1-3 b: cdefg",
+                           "2-9 c: ccccccccc",
+                       };
+
+            var result = tested.CountCorrectPasswords(data);
+
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Solve2TestFromWeb()
+        {
+            var tested = new Day2();
+            var data = new[]
+                       {
+                           "1-3 a: abcde",
+                           "1-3 b: cdefg",
+                           "2-9 c: ccccccccc",
+                       };
+
+            var result = tested.CountCorrectPasswords_2(data);
+
+            Assert.Equal(1, result);
+        }
+    }
+}
