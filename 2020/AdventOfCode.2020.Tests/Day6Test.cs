51a96c09ca06b6bbd018c77d764b71c2de8cb139
Jupotter <jupotter@jupotter.eu>
2020-12-08 11:29:35 +0100

    Day 6

---
+++ 2020/AdventOfCode.2020.Tests/Day6Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AdventOfCode2020.Tests
+{
+    public class Day6Test
+    {
+        private const string SampleData = @"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b";
+
+        [Theory]
+        [InlineData("abc", 3)]
+        [InlineData("a\r\nb\r\nc", 0)]
+        [InlineData("ab\r\nac", 1)]
+        [InlineData("a\r\na\r\na\r\na", 1)]
+        [InlineData("b", 1)]
+        public void CountAllTest(string data, int expected)
+        {
+            var result = Day6.CountAll(data);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ 2020/AdventOfCode.2020/Day6.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AoCHelper;
+
+namespace AdventOfCode2020
+{
+    public sealed class Day6 : BaseDay
+    {
+        private readonly string data;
+
+        public Day6()
+        {
+            using var sr = new StreamReader(InputFilePath);
+            data = sr.ReadToEnd();
+        }
+
+        public override string Solve_1()
+        {
+            return SplitData(data).Select(CountUnique).Sum().ToString();
+        }
+
+        public override string Solve_2()
+        {
+            return SplitData(data).Select(CountAll).Sum().ToString();
+        }
+
+        public static int CountUnique(string data)
+        {
+            HashSet<char> unique = new HashSet<char>(data);
+            unique.Remove('\r');
+            unique.Remove('\n');
+            return unique.Count;
+        }
+
+        public static int CountAll(string data)
+        {
+            var   split = data.Split(Environment.NewLine);
+            int[] count = new int[26];
+            foreach (var line in split)
+            {
+                foreach (var c in line)
+                {
+                    count[c - 'a']++;
+                }
+            }
+
+            return count.Count(c => c == split.Length);
+        }
+
+        public static IEnumerable<string> SplitData(string source)
+        {
+            return source.Trim().Split(Environment.NewLine + Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
