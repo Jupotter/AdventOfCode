0f13261957f251f79662ddf3dd1283a1fa5d769c
Jupotter <jupotter@jupotter.eu>
2020-12-09 14:39:45 +0100

    Day 7

---
+++ 2020/AdventOfCode.2020.Tests/Day7Test.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AdventOfCode2020.Tests
+{
+    public class Day7Test
+    {
+        [Fact]
+        public void ParseLine()
+        {
+            const string line = "light red bags contain 1 bright white bag, 2 muted yellow bags.";
+
+            var tested = new Day7();
+
+            tested.ParseData(line);
+
+            var content = tested.BagContent["light red"];
+
+            Assert.Equal(2, content.Count);
+            Assert.Equal(1, content["bright white"]);
+            Assert.Equal(2, content["muted yellow"]);
+        }
+
+        [Fact]
+        public void ParseLineNoOther()
+        {
+            const string line = "dotted black bags contain no other bags.";
+
+            var tested = new Day7();
+
+            tested.ParseData(line);
+
+            var content = tested.BagContent["dotted black"];
+
+            Assert.Empty(content);
+        }
+
+        [Fact]
+        public void FindParentsInSample()
+        {
+            const string data = @"light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.";
+
+            var tested = new Day7();
+            tested.ParseData(data);
+
+            var result = tested.CountParents("shiny gold");
+
+            Assert.Equal(4, result);
+
+        }
+
+        [Fact]
+        public void FindContentsInSample()
+        {
+            const string data = @"light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.";
+
+            var tested = new Day7();
+            tested.ParseData(data);
+
+            var result = tested.CountContent("shiny gold");
+
+            Assert.Equal(32, result);
+
+        }
+
+        [Fact]
+        public void FindContentsInSample2()
+        {
+            const string data = @"shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.";
+
+            var tested = new Day7();
+            tested.ParseData(data);
+
+            var result = tested.CountContent("shiny gold");
+
+            Assert.Equal(126, result);
+
+        }
+    }
+}
---
+++ 2020/AdventOfCode.2020/Day7.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AoCHelper;
+
+namespace AdventOfCode2020
+{
+    public sealed class Day7 : BaseDay
+    {
+        private readonly Dictionary<string, Dictionary<string, int>> bagContent = new();
+
+        public Day7()
+        {
+            using var sr = new StreamReader(InputFilePath);
+            ParseData(sr.ReadToEnd());
+        }
+
+        public IReadOnlyDictionary<string, Dictionary<string, int>> BagContent => bagContent;
+
+        public void ParseData(string data)
+        {
+            bagContent.Clear();
+
+            var regex = new Regex(@"(?<bag>\w+ \w+) bags contain(?:( (?<count>\d+) (?<color>\w+ \w+) bags?(,|.))| no other bags\.)+");
+
+            MatchCollection matches = regex.Matches(data);
+            foreach (Match match in matches)
+            {
+                string bag = match.Groups["bag"].Value;
+                bagContent[bag] = new Dictionary<string, int>();
+                for (var i = 0; i < match.Groups[1].Captures.Count; i++)
+                {
+                    string color = match.Groups["color"].Captures[i].Value;
+                    string count = match.Groups["count"].Captures[i].Value;
+
+                    bagContent[bag].Add(color, int.Parse(count));
+                }
+            }
+        }
+
+        public int CountParents(string bag)
+        {
+            Dictionary<string, List<string>> reversed = ReverseBags();
+
+            HashSet<string> seen    = new();
+            Queue<string>   toCheck = new();
+            toCheck.Enqueue(bag);
+
+            do
+            {
+                string current = toCheck.Dequeue();
+                foreach (string container in reversed[current].Where(c => seen.Add(c)))
+                    toCheck.Enqueue(container);
+            } while (toCheck.Any());
+
+            return seen.Count;
+        }
+
+        public int CountContent(string bag)
+        {
+            int CountContentRec(string current, int times)
+            {
+                int count = 1;
+
+                foreach (var content in bagContent[current])
+                {
+                    count += CountContentRec(content.Key, content.Value);
+                }
+
+                return count * times;
+            }
+
+            return CountContentRec(bag, 1) - 1;
+        }
+
+        private Dictionary<string, List<string>> ReverseBags()
+        {
+            var reversed = new Dictionary<string, List<string>>();
+            foreach (KeyValuePair<string, Dictionary<string, int>> content in bagContent)
+            {
+                string bag = content.Key;
+                if (!reversed.ContainsKey(bag))
+                    reversed[bag] = new List<string>();
+                foreach (string contained in content.Value.Keys)
+                {
+                    if (!reversed.ContainsKey(contained))
+                        reversed[contained] = new List<string>();
+                    reversed[contained].Add(bag);
+                }
+            }
+
+            return reversed;
+        }
+
+        public override string Solve_1() => CountParents("shiny gold").ToString();
+
+        public override string Solve_2() => CountContent("shiny gold").ToString();
+    }
+}
