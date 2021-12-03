a95d7fa652fae92c59dcb2f153f8025edcac1587
Jupotter <jupotter@jupotter.eu>
2021-12-03 11:29:23 +0100

    Day03

---
+++ 2021/AdventOfCode.2021.Tests/Day03Test.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode2021.Tests;
+
+public class Day03Test {
+
+    private readonly uint[] exampleData = new [] {
+            0b00100U,
+            0b11110U,
+            0b10110U,
+            0b10111U,
+            0b10101U,
+            0b01111U,
+            0b00111U,
+            0b11100U,
+            0b10000U,
+            0b11001U,
+            0b00010U,
+            0b01010U,
+        };
+
+    [Fact]
+    public void MostCommonBitTest()
+    {
+        var expected = 0b10110U;
+
+        var result = Day03.GetMostCommonBitByColumn(exampleData, 5);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(0b10110U, 5, 0b01001U)]
+    [InlineData(0b11111U, 5, 0b00000U)]
+    [InlineData(0b00000U, 5, 0b11111U)]
+    public void ReverseTest(uint input, int columnCount, uint expected)
+    {
+        var result = Day03.Reverse(input, columnCount);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void GetO2GeneratorRating()
+    {
+        var expected = 0b10111U;
+        var result = Day03.GetO2GeneratorRating(exampleData, 5);
+        Assert.Equal(expected, result);
+    }
+    [Fact]
+    public void GetCO2ScrubberRating()
+    {
+        var expected = 0b01010U;
+        var result = Day03.GetCO2ScrubberRating(exampleData, 5);
+        Assert.Equal(expected, result);
+    }
+}
---
+++ 2021/AdventOfCode.2021/Day03.cs
@@ -0,0 +1,129 @@
+﻿namespace AdventOfCode2021;
+
+public class Day03 : BaseDay
+{
+    private List<uint> _input = new List<uint>();
+    private int _columnCount = 0;
+
+    public Day03()
+    {
+        if (!File.Exists(InputFilePath))
+            return;
+
+        var content = File.ReadAllLines(InputFilePath);
+        _columnCount = content[0].Trim().Length;
+
+        _input = content.Select(l => Convert.ToUInt32(l.Trim(), 2)).ToList();
+
+    }
+
+    public static uint GetMostCommonBitByColumn(IEnumerable<uint> array, int columnCount)
+    {
+        int[] count = new int[columnCount];
+        foreach (uint value in array)
+        {
+            for (int i = 0; i < columnCount; i++)
+            {
+                uint bit = 1U << i;
+
+                if ((value & bit) == 0)
+                {
+                    count[i]++;
+                } 
+                else
+                {
+                    count[i]--;
+                }
+            }
+        }
+
+        uint result = 0;
+        for (int i = 0; i < columnCount; i++)
+        {
+            result |= (count[i] > 0 ? 0U : 1U) << i;
+        }
+
+        return result;
+    }
+
+    public static uint GetMostCommonBitByColumn(IEnumerable<uint> array, int column,  int columnCount)
+    {
+        int count = 0;
+        foreach (uint value in array)
+        {
+                uint bit = 1U << column;
+
+                if ((value & bit) == 0)
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            
+        }
+
+        return count > 0 ? 0U : 1U;
+    }
+
+    public static uint GetO2GeneratorRating(IEnumerable<uint> array, int columnCount)
+    {
+        List<uint> remaining = array.ToList();
+
+        int c = columnCount-1;
+        while (remaining.Count > 1)
+        {
+            var mask = 1U << c;
+            var mostCommon = GetMostCommonBitByColumn(remaining, c, columnCount);
+
+            for (int i = remaining.Count() - 1; i >= 0; i--)
+            {
+                if (((remaining[i] ^ (mostCommon << c)) & mask) != 0)
+                    remaining.RemoveAt(i);
+            }
+
+            c--;
+        }
+
+        return remaining[0];
+    }
+
+    public static uint GetCO2ScrubberRating(IEnumerable<uint> array, int columnCount)
+    {
+        List<uint> remaining = array.ToList();
+
+        int c = columnCount - 1;
+        while (remaining.Count > 1)
+        {
+            var mask = 1U << c;
+            var mostCommon = GetMostCommonBitByColumn(remaining, c, columnCount);
+
+            for (int i = remaining.Count() - 1; i >= 0; i--)
+            {
+                if (((remaining[i] ^ (mostCommon << c)) & mask) == 0)
+                    remaining.RemoveAt(i);
+            }
+
+            c--;
+        }
+
+        return remaining[0];
+    }
+
+    public static uint Reverse (uint input, int columnCount) => (uint)(~input & ((1 << columnCount) - 1));
+
+    public override ValueTask<string> Solve_1()
+    {
+        var result = GetMostCommonBitByColumn(_input, _columnCount);
+        var reverse = Reverse(result, _columnCount);
+        return new((result * reverse).ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var CO2 = GetCO2ScrubberRating(_input, _columnCount);
+        var O2 = GetO2GeneratorRating(_input, _columnCount);
+        return new((CO2 * O2).ToString());
+    }
+}
