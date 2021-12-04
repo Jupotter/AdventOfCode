0e80f0a6f2608ce3016ca149f778ea84cba3efbb
Jupotter <jupotter@jupotter.eu>
2021-12-04 16:12:22 +0100

    Day 04

---
+++ 2021/AdventOfCode.2021.Tests/Day04Test.cs
@@ -0,0 +1,119 @@
+﻿namespace AdventOfCode2021.Tests;
+
+public class Day04Test
+{
+    private const string TestData = @"22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+";
+
+
+    [Fact]
+    public void TestParseBingoBoard()
+    {
+        var expected = new[,] {
+            {22, 13, 17, 11, 0},
+            {8, 2, 23, 4, 24 },
+            {21, 9, 14, 16, 7},
+            {6, 10, 3, 18, 5},
+            {1, 12, 20, 15, 19}
+        };
+        var tested = Day04.BingoBoard.Parse(TestData);
+
+        Assert.False(tested.IsValid());
+        CompareBoardToExpected(expected, tested);
+    }
+
+    [Fact]
+    public void CheckBoardValid1()
+    {
+        var tested = Day04.BingoBoard.Parse(TestData);
+        tested[3, 0].Marked = true;
+        tested[3, 1].Marked = true;
+        tested[3, 2].Marked = true;
+        tested[3, 3].Marked = true;
+        tested[3, 4].Marked = true;
+
+        Assert.True(tested.IsValid());
+    }
+
+    [Fact]
+    public void CheckBoardValid2()
+    {
+        var tested = Day04.BingoBoard.Parse(TestData);
+        tested[0, 3].Marked = true;
+        tested[1, 3].Marked = true;
+        tested[2, 3].Marked = true;
+        tested[3, 3].Marked = true;
+        tested[4, 3].Marked = true;
+
+        Assert.True(tested.IsValid());
+    }
+
+    [Fact]
+    public void TestMarkNumber()
+    {
+        var tested = Day04.BingoBoard.Parse(TestData);
+
+        tested.Mark(14);
+
+        Assert.True(tested[2, 2].Marked);
+    }
+
+    [Fact]
+    public void TestValidateWithMarkNumber()
+    {
+        var tested = Day04.BingoBoard.Parse(TestData);
+
+        tested.Mark(14);
+        tested.Mark(23);
+        tested.Mark(2);
+        tested.Mark(17);
+        tested.Mark(3);
+        tested.Mark(20);
+
+        Assert.True(tested.IsValid());
+    }
+
+    [Fact]
+    public void TestParseMultipleBoards()
+    {
+        var text =
+            @"
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7";
+
+        var tested = Day04.ParseBoards(text);
+
+        Assert.Equal(3, tested.Count);
+    }
+
+    private static void CompareBoardToExpected(int[,] expected, Day04.BingoBoard tested)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Assert.Equal(expected[i, j], tested[i, j].Number);
+                Assert.False(tested[i, j].Marked);
+            }
+        }
+    }
+}
---
+++ 2021/AdventOfCode.2021/Day04.cs
@@ -0,0 +1,181 @@
+﻿namespace AdventOfCode2021;
+
+public class Day04 : BaseDay
+{
+    private List<int> numbers = new();
+    private List<BingoBoard> boards = new();
+
+    public Day04()
+    {
+        if (!File.Exists(InputFilePath))
+            return;
+
+        using var sr = new StreamReader(InputFilePath);
+        
+        var numbersLine = sr.ReadLine();
+        numbers = numbersLine.Split(',').Select(int.Parse).ToList();
+        boards = ParseBoards(sr.ReadToEnd());
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var winners = GetWinningBoard(numbers, boards);
+        int result = GetScore(winners.lastNumber, winners.board);
+
+        return new(result.ToString());
+    }
+
+    private static int GetScore(int lastNumber, BingoBoard board)
+    {
+        var unmarkedSum = board.Tiles.Where(t => !t.Marked).Select(t => t.Number).Sum();
+        var result = lastNumber * unmarkedSum;
+        return result;
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var winners = GetLastWinningBoard(numbers, boards);
+        int result = GetScore(winners.lastNumber, winners.board);
+
+        return new(result.ToString());
+    }
+
+    public (int lastNumber, BingoBoard board) GetWinningBoard(IEnumerable<int> numbers, IEnumerable<BingoBoard> boards) 
+    {
+        foreach (int i in numbers)
+        {
+            foreach(BingoBoard board in boards)
+            {
+                board.Mark(i);
+                if (board.IsValid())
+                {
+                    return (i, board);
+                }
+            }
+        }
+
+        throw new InvalidOperationException();
+    }
+
+    public (int lastNumber, BingoBoard board) GetLastWinningBoard(IEnumerable<int> numbers, IEnumerable<BingoBoard> boards)
+    {
+        var remainingBoards = boards.ToList();
+        var toRemove = new List<BingoBoard>();
+
+        int lastNumber = 0;
+        BingoBoard? lastWin = null;
+        foreach (int i in numbers)
+        {
+            toRemove.Clear();
+            foreach (BingoBoard board in remainingBoards)
+            {
+                board.Mark(i);
+                if (board.IsValid())
+                {
+                    lastNumber = i;
+                    lastWin = board;
+                    toRemove.Add(board);
+                }
+            }
+
+            foreach (BingoBoard board in toRemove)
+                remainingBoards.Remove(board);
+
+            if (!remainingBoards.Any())
+                break;
+        }
+
+        System.Diagnostics.Debug.Assert(lastWin != null);
+        return (lastNumber, lastWin);
+    }
+
+    public class BingoBoard
+    {
+        public record class Tile(int Number)
+        {
+            public bool Marked { get; set; } = false;
+        }
+
+        public IEnumerable<Tile> Tiles => tiles.Values;
+
+        private readonly Tile[,] board = new Tile[5, 5];
+
+        private readonly Dictionary<int, Tile> tiles = new Dictionary<int, Tile>();
+
+        public Tile this[int x, int y]
+        {
+            get => board[x, y];
+            private set
+            {
+                if (board[x, y] != null)
+                    tiles.Remove(board[x, y].Number);
+                board[x, y] = value;
+                tiles[value.Number] = value;
+            }
+        }
+
+        public bool IsValid()
+        {
+            bool[] columns = new bool[5];
+            for (int i = 0; i < columns.Length; i++)
+            {
+                columns[i] = true;
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                bool line = true;
+                for (int j = 0; j < 5; j++)
+                {
+                    if (!this[i, j].Marked)
+                    {
+                        line = false;
+                        columns[j] = false;
+                    }
+                }
+                if (line)
+                    return true;
+            }
+            return columns.Any(b => b);
+        }
+
+        public static BingoBoard Parse(string description)
+        {
+            var board = new BingoBoard();
+
+            var lines = description.Split(Environment.NewLine);
+            int i = 0;
+            foreach(var line in lines)
+            {
+                var numbers=  line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (numbers.Length == 0)
+                    continue;
+                var j = 0;
+                foreach (var number in numbers)
+                {
+                    if (int.TryParse(number, out var parsed))
+                    {
+                        board[i, j] = new Tile(parsed);
+                        j++;
+                    }
+                }
+                i++;
+            }
+
+            return board;
+        }
+
+        public void Mark(int number)
+        {
+            if (tiles.ContainsKey(number))
+                tiles[number].Marked = true;
+        }
+    }
+
+    public static List<BingoBoard> ParseBoards(string input)
+    {
+        var split = input.Split("\r\n\r\n", StringSplitOptions.RemoveEmptyEntries);
+
+        return split.Select(x => BingoBoard.Parse(x)).ToList();
+    }
+}
