51d003331ae13b839505317bf96b165e8a17df2d
Jupotter <jupotter@jupotter.eu>
2021-12-06 17:55:09 +0100

    Day05

---
+++ 2021/AdventOfCode.2021.Tests/Day05Test.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode2021.Tests;
+
+public class Day05Test
+{
+    [Fact]
+    public void TestParseLines()
+    {
+        var data = @"0,9 -> 5,9
+                     8,0 -> 0,8
+                     9,4 -> 3,4
+                     2,2 -> 2,1
+                     7,0 -> 7,4
+                     6,4 -> 2,0
+                     0,9 -> 2,9
+                     3,4 -> 1,4
+                     0,0 -> 8,8
+                     5,5 -> 8,2";
+
+        var expected = new List<Day05.VentLine>()
+        {
+            new Day05.VentLine(0, 9, 5, 9),
+            new Day05.VentLine(8,0,0,8),
+            new Day05.VentLine(9,4,3,4),
+            new Day05.VentLine(2,2,2,1),
+            new Day05.VentLine(7,0,7,4),
+            new Day05.VentLine(6,4,2,0),
+            new Day05.VentLine(0,9,2,9),
+            new Day05.VentLine(3,4,1,4),
+            new Day05.VentLine(0,0,8,8),
+            new Day05.VentLine(5,5,8,2),
+        };
+
+        var tested = Day05.ParseLines(data);
+        Assert.Equal(expected, tested);
+    }
+}
---
+++ 2021/AdventOfCode.2021/Day05.cs
@@ -0,0 +1,139 @@
+﻿namespace AdventOfCode2021;
+
+public class Day05 : BaseDay
+{
+    public record class VentLine(int X1, int Y1, int X2, int Y2)
+    {
+        public VentLine ((int x, int y) start, (int x, int y) end) : this(start.x, start.y, end.x, end.y) { }
+    };
+
+    private List<VentLine> _input = new List<VentLine>();
+
+    public Day05()
+    {
+        if (!File.Exists(InputFilePath))
+            return;
+
+        _input = ParseLines(File.ReadAllText(InputFilePath));
+    }
+    public static List<VentLine> ParseLines(string input)
+    {
+        var result = new List<VentLine>();
+        var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var line in lines)
+        {
+            var coordinates = line.Split("->", StringSplitOptions.TrimEntries);
+            var startStr = coordinates[0];
+            var endStr = coordinates[1];
+
+            result.Add(new VentLine(ParseCoordinate(startStr), ParseCoordinate(endStr)));
+        }
+
+        return result;
+    }
+
+    private static (int X, int Y) ParseCoordinate(string input)
+    {
+        var split = input.Split(',');
+
+        return (int.Parse(split[0]), int.Parse(split[1]));
+
+    }
+
+    public static void PlaceStraightLines(int[,] array, IEnumerable<VentLine> lines)
+    {
+        foreach (var line in lines.Where(l => l.X1 == l.X2))
+        {
+            var start = line.Y1;
+            var end = line.Y2;
+            if (line.Y1 > line.Y2)
+            {
+                start = line.Y2;
+                end = line.Y1;
+            }
+
+            for (int i = start; i <= end; i++)
+            {
+                array[line.X1, i] += 1;
+            }
+        }
+
+        foreach (var line in lines.Where(l => l.Y1 == l.Y2))
+        {
+            var start = line.X1;
+            var end = line.X2;
+            if (line.X1 > line.X2)
+            {
+                start = line.X2;
+                end = line.X1;
+            }
+
+            for (int i = start; i <= end; i++)
+            {
+                array[i, line.Y1] += 1;
+            }
+        }
+    }
+
+    public static void PlaceDiagonals(int[,] array, IEnumerable<VentLine> lines)
+    {
+        foreach (var line in lines.Where(l => l.X1 != l.X2 && l.Y1 != l.Y2))
+        {
+            if (line.X1 < line.X2 && line.Y1 < line.Y2
+                || line.X1 > line.X2 && line.Y1 > line.Y2)
+            {
+                var startX = Math.Min(line.X1, line.X2);
+                var startY = Math.Min(line.Y1, line.Y2);
+                var length = Math.Abs(line.X1 - line.X2);
+                for (int i = 0; i <= length; i++)
+                {
+                    array[startX + i, startY + i]++;
+                }
+            } 
+            else
+            {
+
+                var startX = Math.Min(line.X1, line.X2);
+                var startY = Math.Max(line.Y1, line.Y2);
+                var length = Math.Abs(line.X1 - line.X2);
+                for (int i = 0; i <= length; i++)
+                {
+                    array[startX + i, startY - i]++;
+                }
+            }
+        }
+    }
+
+    public static int CountMultiples(int[,] array)
+    {
+        int total = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (array[i, j] > 1)
+                {
+                    total++;
+                }
+            }
+        }
+        return total;
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var array = new int[1000, 1000];
+        PlaceStraightLines(array, _input);
+        var total = CountMultiples(array);
+        return new ValueTask<string>(total.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var array = new int[1000, 1000];
+        PlaceStraightLines(array, _input);
+        PlaceDiagonals(array, _input);
+        var total = CountMultiples(array);
+        return new ValueTask<string>(total.ToString());
+    }
+}
