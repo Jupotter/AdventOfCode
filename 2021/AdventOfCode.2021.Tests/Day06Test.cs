0816672aefae78e9518c20ad976e53bf0100f505
Jupotter <jupotter@jupotter.eu>
2021-12-06 18:31:28 +0100

    Day06

---
+++ 2021/AdventOfCode.2021.Tests/Day06Test.cs
@@ -0,0 +1,24 @@
+﻿namespace AdventOfCode2021.Tests;
+
+public class Day06Test {
+
+    [Theory]
+    [InlineData(new[] { 0 }, new[] { 6, 8 })]
+    [InlineData(new[] { 3 }, new[] { 2 })]
+    [InlineData(new[] { 3, 0 }, new[] { 2, 6, 8 })]
+    public void TestGrow(int[] input, int[] expected)
+    {
+        var result = Day06.Grow(input);
+        Assert.All(result, r => Assert.Contains(r, expected));
+    }
+
+    [Fact]
+    public void TestGrowFast()
+    {
+        var data = new List<int> { 3, 4, 3, 1, 2 };
+        var result = Day06.GrowFast(data, 18);
+        Assert.Equal(26, result);
+        result = Day06.GrowFast(data, 80);
+        Assert.Equal(5934, result);
+    }
+}
---
+++ 2021/AdventOfCode.2021/Day06.cs
@@ -0,0 +1,63 @@
+﻿namespace AdventOfCode2021;
+
+public class Day06 : BaseDay
+{
+
+    List<int> _input = new List<int>();
+
+    public Day06()
+    {
+        if (!File.Exists(InputFilePath))
+            return;
+
+        _input = File.ReadAllText(InputFilePath).Split(',').Select(int.Parse).ToList();
+    }
+    public static IEnumerable<int> Grow(IEnumerable<int> fishes)
+    {
+        return fishes.SelectMany(f =>
+        {
+            if (f == 0)
+            {
+                return new int[] { 6, 8 };
+            }
+            else
+            {
+                return new int[] { f - 1 };
+            }
+        });
+    }
+
+    public static long GrowFast(List<int> initial, int iterations)
+    {
+        long[] fishCount = new long[9];
+        for (int i = 0; i <= 8; i++)
+        {
+            fishCount[i] = initial.Count(f => f == i);
+        }
+
+        foreach (var _ in Enumerable.Range(0, iterations))
+        {
+            var zero = fishCount[0];
+            for (int i = 1; i <= 8; i++)
+            {
+                fishCount[i-1] = fishCount[i];
+            }
+
+            fishCount[6] += zero;
+            fishCount[8] = zero;
+        }
+
+        return fishCount.Sum();
+    }
+
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new ValueTask<string>(GrowFast(_input, 80).ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new ValueTask<string>(GrowFast(_input, 256).ToString());
+    }
+}
