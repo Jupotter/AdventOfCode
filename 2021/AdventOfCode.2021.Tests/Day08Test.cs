2d2daf1d7ee7c99c311e0ec689e9195038e61eb3
Jupotter <jupotter@jupotter.eu>
2021-12-10 12:00:59 +0100

    Day08

---
+++ 2021/AdventOfCode.2021.Tests/Day08Test.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace AdventOfCode2021.Tests;
+
+public class Day08Test { 
+
+    [Fact]
+    public void TestParseInputLine()
+    {
+        var expected = new Day08.DisplayInfo(new List<string> { "acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab" },
+            new List<string> { "cdfeb", "fcadb", "cdfeb", "cdbaf" });
+        var line = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf";
+        var result = Day08.ParseInputLine(line);
+
+        Assert.Equal(expected.Input, result.Input);
+        Assert.Equal(expected.Output, result.Output);
+    }
+
+    [Theory]
+    [InlineData("|fdgacbe cefdb cefbgd gcbe", 2)]
+    [InlineData("|fcgedb cgb dgebacf gc", 3)]
+    [InlineData("|ed bcgafe cdgba cbgef", 1)]
+    [InlineData("|edfga bcgafe cdgba cbgef", 0)]
+    public void TestCountEasyOutput(string input, int expected)
+    {
+        var line = Day08.ParseInputLine(input);
+
+        int result = Day08.CountEasyInput(line);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void TestDecodeDisplay()
+    {
+        var tested = new Day08.SegmentDisplay();
+        var input = Day08.ParseInputLine("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf");
+
+        foreach (var data in input.Input.Concat(input.Output))
+        {
+            tested.AddInput(data);
+        }
+
+        Assert.True(tested.IsDecoded);
+        Assert.Equal(5, tested.GetValue("cdfbe"));
+    }
+
+    [Theory]
+    [InlineData("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf", 5353)]
+    [InlineData("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe", 8394)]
+    [InlineData("edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc", 9781)]
+    public void TestDecodeInput(string data, int expected)
+    {
+        var input = Day08.ParseInputLine(data);
+
+        var result = Day08.Decode(input);
+        Assert.Equal(expected, result);
+    }
+}
---
+++ 2021/AdventOfCode.2021/Day08.cs
@@ -0,0 +1,158 @@
+﻿using System.Linq;
+
+namespace AdventOfCode2021;
+
+public class Day08 : BaseDay
+{
+    public record struct DisplayInfo(List<string> Input, List<string> Output);
+
+    public class SegmentDisplay
+    {
+        // Store wires possible for each segment
+        //  00
+        // 1  2
+        // 1  2
+        //  33
+        // 4  5
+        // 4  5
+        //  66
+        private List<char>[] wires = Enumerable.Range(0, 7).Select(_ => new List<char>("abcdefg")).ToArray();
+
+        public bool IsDecoded => wires.All(x => x.Count == 1);
+
+        public void AddInput(string input)
+        {
+            if (IsDecoded)
+                return;
+            var except = "abcdefg".Except(input).ToHashSet();
+            switch (input.Length)
+            {
+                case 2: // number is 1
+                    wires[2].RemoveAll(x => except.Contains(x));
+                    wires[5].RemoveAll(x => except.Contains(x));
+                    break;
+                case 3: // number is 7
+                    wires[0].RemoveAll(x => except.Contains(x));
+                    wires[2].RemoveAll(x => except.Contains(x));
+                    wires[5].RemoveAll(x => except.Contains(x));
+                    break;
+                case 4: // number is 4
+                    wires[1].RemoveAll(x => except.Contains(x));
+                    wires[3].RemoveAll(x => except.Contains(x));
+                    wires[2].RemoveAll(x => except.Contains(x));
+                    wires[5].RemoveAll(x => except.Contains(x));
+                    break;
+                case 5: // number is 2, 3 or 5
+                    wires[0].RemoveAll(x => except.Contains(x));
+                    wires[3].RemoveAll(x => except.Contains(x));
+                    wires[6].RemoveAll(x => except.Contains(x));
+                    break;
+                case 6: // number is 0, 6 or 9
+                    wires[0].RemoveAll(x => except.Contains(x));
+                    wires[1].RemoveAll(x => except.Contains(x));
+                    wires[5].RemoveAll(x => except.Contains(x));
+                    wires[6].RemoveAll(x => except.Contains(x));
+                    break;
+                case 7: // number is 8
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(input), input);
+            }
+
+            System.Diagnostics.Debug.Assert(!wires.Any(w => w.Count == 0), "No segment should be empty");
+            if (IsDecoded)
+                return;
+
+            for (int i = 0; i < 7; i++)
+            {
+                if (wires[i].Count == 1)
+                {
+                    for (int j = 0; j < 7; j++)
+                    {
+                        if (i != j)
+                        {
+                            wires[j].Remove(wires[i][0]);
+
+                            System.Diagnostics.Debug.Assert(!wires.Any(w => w.Count == 0), "No segment should be empty");
+                        }
+                    }
+                }
+            }
+
+        }
+
+        public int GetValue(string input)
+        {
+            switch (input.Length)
+            {
+                case 2:
+                    return 1;
+                case 3:
+                    return 7;
+                case 4:
+                    return 4;
+                case 7:
+                    return 8;
+                case 5: // number is 2, 3 or 5
+                    if (input.Contains(wires[4][0]))
+                        return 2;
+                    else if (input.Contains(wires[1][0]))
+                        return 5;
+                    else return 3;
+
+                case 6: // number is 0, 6 or 9
+                    if (!input.Contains(wires[3][0]))
+                        return 0;
+                    else if (input.Contains(wires[2][0]))
+                        return 9;
+                    else return 6;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(input), input);
+            }
+        }
+    }
+
+    private readonly List<DisplayInfo> _input;
+
+    public Day08()
+    {
+
+        _input = File.ReadAllText(InputFilePath).Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(ParseInputLine).ToList();
+    }
+    public static DisplayInfo ParseInputLine(string line)
+    {
+        var parts = line.Split('|');
+        var inputs = parts[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        var output = parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        return new DisplayInfo(inputs.ToList(), output.ToList());
+    }
+
+    public static int CountEasyInput(DisplayInfo line)
+    {
+        return line.Output.Count(l => l.Length == 2 || l.Length == 4 || l.Length == 7 || l.Length == 3);
+    }
+
+    public static int Decode(DisplayInfo input)
+    {
+        var display = new SegmentDisplay();
+        foreach (var data in input.Input.Concat(input.Output))
+        {
+            display.AddInput(data);
+        }
+
+        return input.Output.Aggregate(0, (acc, val) => acc * 10 + display.GetValue(val));
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new ValueTask<string>(_input.Sum(CountEasyInput).ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var result = _input.Select(x => Decode(x)).Sum();
+        return new ValueTask<string>(result.ToString());
+    }
+
+}
