c3f06fb2a2954ccaab4b3adab731777423f6e5c9
Jupotter <jupotter@jupotter.eu>
2022-12-02 10:15:29 +0100

    2022 Day01

---
+++ 2022/AdventOfCode.2022.Tests/Day01Should.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+
+namespace AdventOfCode2022.Tests;
+
+[TestFixture]
+public class Day01Should
+{
+    private const string SampleData = @"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+    [Test]
+    public async Task SolveSampleData()
+    {
+        var tested = new Day01(SampleData);
+        var result = await tested.Solve_1();
+
+        result.Should().Be("24000");
+    }
+
+    [Test]
+    public async Task SolveSampleData2()
+    {
+        var tested = new Day01(SampleData);
+        var result = await tested.Solve_2();
+
+        result.Should().Be("45000");
+    }
+}
---
+++ 2022/AdventOfCode.2022/Day01.cs
@@ -0,0 +1,49 @@
+﻿namespace AdventOfCode2022;
+
+public sealed class Day01 : BaseDay
+{
+    private readonly List<int> carried = new();
+
+    public Day01()
+    {
+        using var reader = new StreamReader(InputFilePath);
+        LoadText(reader);
+    }
+    public Day01(string content)
+    {
+        using var reader = new StringReader(content);
+        LoadText(reader);
+    }
+
+    private void LoadText(TextReader reader)
+    {
+        var line = reader.ReadLine();
+        int i    = 0;
+        do
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                i++;
+            }
+            else
+            {
+                var value = int.Parse(line);
+                if (carried.Count <= i)
+                {
+                    carried.Add(0);
+                }
+
+                carried[i] += value;
+            }
+
+            line = reader.ReadLine();
+        } while (line != null);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new ValueTask<string>(carried.Max().ToString());
+    }
+
+    public override ValueTask<string> Solve_2() => new(carried.OrderDescending().Take(3).Sum().ToString());
+}
